f47ea66c24fec1c1e808f822d8c5515e2e5f45a3
mdctenga <mdctenga@gmail.com>
2015-08-08 19:20:45 -1000

    Login successful. Redirect to dashboard.

--- client/views/layout.js
+++ client/views/layout.js
@@ -7,10 +7,20 @@ Router.route('/', function () {
   this.render('main');
 });
 
+Router.onBeforeAction(function () {
+    if (! Meteor.userId()) {
+      if (Meteor.loggingIn()) {
+        Router.go('/dashboard');
+      } else {
+        Router.go('/');
+      }
+    }
+  });
+
 Router.route('/dashboard', function () {
   this.render('dashboard');
 });
 
-Router.route('login', function () {
-  this.render('login');
-});
+// Router.route('login', function () {
+//   this.render('login');
+// });
