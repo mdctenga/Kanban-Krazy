e164885098a7ff7c5c57011f90f7c7cd1de3a2fe
mdctenga <mdctenga@gmail.com>
2015-08-13 19:03:48 -1000

    Clean up for quick test

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ Meteor.startup(function(){
   var sean = "xkFKifpjZu78zSa9h";
   var matt = "z5fnuu8ZosbjqrHne";
 
-  if (KanbanCollection.find().fetch().length > 0) {
+  if (KanbanCollection.find().fetch().length === 0) {
     KanbanCollection.remove({});
     var array = [{user: matt, message: 'hell0'},
                  {user: sean, message: 'butter mochi'},
